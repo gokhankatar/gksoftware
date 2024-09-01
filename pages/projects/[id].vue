accb74afa2de2b9100b95c3bf2ebf91b7523d8b1
gokhankatar <katar_gokhan@hotmail.com>
2024-09-02 00:28:56 +0300

    added back-btn

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,12 +1,32 @@
 <template>
-    <div class="bg-primary pa-3">
-        {{ route.params }}
-    </div>
-    <div class="bg-primary pa-3">
-        {{ route.query }}
-    </div>
+  <NuxtLinkLocale
+    class="back-link pa-3 rounded-lg transition"
+    to="/projects"
+    variant="tonal"
+    rounded="lg"
+    >go back</NuxtLinkLocale
+  >
+  <div class="bg-primary pa-3 mt-15">
+    {{ route.params }}
+  </div>
+  <div class="bg-primary pa-3">
+    {{ route.query }}
+  </div>
 </template>
 <script lang="ts" setup>
 const route = useRoute();
 </script>
-<style scoped></style>
+<style scoped>
+@import url("/assets/css/main.css");
+
+.back-link {
+  text-decoration: none;
+  color: red;
+  border: 2px solid red;
+  float: right;
+}
+.back-link:hover {
+  background-color: red;
+  color: #fff;
+}
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,14 +1,27 @@
 <template>
   <h3 class="text-h3">projects</h3>
-  <hr class="my-3">
+  <hr class="my-3" />
   <div class="d-flex flex-column ga-2">
-    <v-card v-for="item of data" :key="item.id" class="card d-flex flex-column align-start ga-2 pa-2 rounded-lg">
+    <v-card
+      v-for="item of data"
+      :key="item.id"
+      class="card d-flex flex-column align-start ga-2 pa-2 rounded-lg"
+    >
       <v-card-title>{{ item.title }}</v-card-title>
       <v-card-text>{{ item.description }}</v-card-text>
-      <NuxtLinkLocale class="link rounded-lg pa-3"
-        :to="{ path: `/projects/${item.id}`, query: { title: item.title, description: item.description, technologies: item.technologies } }">
-        see this
-        project</NuxtLinkLocale>
+      <NuxtLinkLocale
+        class="link rounded-lg pa-3"
+        :to="{
+          path: `/projects/${item.id}`,
+          query: {
+            title: item.title,
+            description: item.description,
+            technologies: item.technologies,
+          },
+        }"
+      >
+        see this project</NuxtLinkLocale
+      >
     </v-card>
   </div>
 </template>
