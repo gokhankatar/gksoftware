576ff667992406f43eb935d6511ae2de7b040401
Gökhan Katar <katar_gokhan@hotmail.com>
2024-09-01 15:54:11 +0300

    added projects api endpoint

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,27 @@
 <template>
   <h3 class="text-h3">projects</h3>
+  <hr class="my-3">
+  <div class="d-flex flex-column ga-2">
+    <v-card v-for="item of data" class="card d-flex flex-column align-start ga-2 pa-2 rounded-lg">
+      <v-card-title>{{ item.title }}</v-card-title>
+      <v-card-text>{{ item.description }}</v-card-text>
+      <a class="link rounded-lg pa-3" :href="item.link" target="_blank">see this project</a>
+    </v-card>
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style scoped></style>
+<script lang="ts" setup>
+const { data } = await useFetch("/api/projects");
+</script>
+<style scoped>
+.card {
+  position: relative;
+}
+
+.link {
+  position: absolute;
+  bottom: 2%;
+  right: 2%;
+  text-decoration: none;
+  border: 1px solid;
+}
+</style>
---
+++ server/api/projects.ts
@@ -0,0 +1,120 @@
+export default eventHandler((event) => {
+  // fotografları da ekleyebilmenin yolunu bul
+  const repositories = [
+    {
+      title: "Follow Yourself",
+      description:
+        "A web application that allows you to track your activities.",
+      link: "https://followyourself.net/",
+      technologies: [
+        "vue",
+        "vue-router",
+        "i18n",
+        "vuetify",
+        "vuex",
+        "gsap",
+        "netlify",
+      ],
+    },
+    {
+      title: "Get Country",
+      description:
+        "A web application that contains a lot of information about all countries in the world",
+      link: "https://www.getcountry.net/",
+      technologies: ["vue", "vue-router", "gsap", "netlify"],
+    },
+    {
+      title: "Personal Academician Site",
+      description:
+        "It is a full stack web application where an academic can share, add, delete and update many of his/her activities such as his/her studies, blog posts, CV. He/she can do these only by logging into the admin panel interface. It is made upon request.",
+      link: "https://esrefugurcelik.com/",
+      technologies: [
+        "vue",
+        "vite",
+        "vuetify",
+        "vue-router",
+        "pinia",
+        "i18n",
+        "momentJs",
+        "html2pdf",
+        "firebase",
+        "netlify",
+      ],
+    },
+    {
+      title: "Crypto Tracker",
+      description:
+        "A web application where every user can add and track any cryptocurrency they want to their watchlist.",
+      link: "https://github.com/gokhankatar/crypto-tracker",
+      technologies: [
+        "vue",
+        "vue-router",
+        "vuetify",
+        "vite",
+        "axios",
+        "pinia",
+        "firebase",
+      ],
+    },
+    {
+      title: "Food App",
+      description: "A basic cafeteria application",
+      link: "https://github.com/gokhankatar/crypto-tracker",
+      technologies: [
+        "vue",
+        "vue-router",
+        "vite",
+        "vuetify",
+        "pinia",
+        "momentJs",
+        "firebase",
+      ],
+    },
+    {
+      title: "Blog App",
+      description: "A simple blog application",
+      link: "https://github.com/gokhankatar/blog-app",
+      technologies: [
+        "vue",
+        "vue-router",
+        "vite",
+        "vuetify",
+        "pinia",
+        "axios",
+        "firebase",
+      ],
+    },
+    {
+      title: "Merchantile App",
+      description:
+        "It is a trading application developed with front-end and back-end. Auto logout. Add/delete/update product. You can save your products, favorite them or mark them as sold and display them on your relevant pages. At the same time, the total of your sold products and your total earnings will be displayed on your sold page. View products from relevant categories. View the products you have uploaded to the platform together on your 'My Products' page",
+      link: "https://github.com/gokhankatar/merchantile-app",
+      technologies: [
+        "vue",
+        "vue-router",
+        "vuex",
+        "vite",
+        "firebase",
+        "rest API",
+        "axios",
+        "vuetify",
+      ],
+    },
+    {
+      title: "Chat App",
+      description: "A basic chat application",
+      link: "https://github.com/gokhankatar/chat-app",
+      technologies: [
+        "vue",
+        "vite",
+        "vue-router",
+        "pinia",
+        "momentJs",
+        "vuetify",
+        "firebase",
+      ],
+    },
+  ];
+
+  return repositories;
+});
