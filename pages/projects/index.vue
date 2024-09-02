1b25b95e2d4f5cd502ef4f6cc2574ab03594a27a
gokhankatar <katar_gokhan@hotmail.com>
2024-09-02 19:46:39 +0300

    updated projects endpoint and component

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,44 +1,63 @@
 <template>
   <h3 class="text-h3">projects</h3>
-  <hr class="my-3" />
-  <div class="d-flex flex-column ga-2">
-    <v-card
-      v-for="item of data"
-      :key="item.id"
-      class="card d-flex flex-column align-start ga-2 pa-2 rounded-lg"
-    >
-      <v-card-title>{{ item.title }}</v-card-title>
-      <v-card-text>{{ item.description }}</v-card-text>
-      <NuxtLinkLocale
-        class="link rounded-lg pa-3"
-        :to="{
-          path: `/projects/${item.id}`,
-          query: {
-            title: item.title,
-            description: item.description,
-            link: item.link,
-            technologies: item.technologies,
-          },
-        }"
+  <hr class="mt-3 mb-10" />
+  <v-row class="d-flex">
+    <v-col cols="12" sm="6" md="4" v-for="item of data" :key="item.id">
+      <v-card
+        class="card cursor-pointer transition d-flex flex-column align-start ga-2 pa-2 rounded-lg"
+        height="200"
       >
-        see this project</NuxtLinkLocale
-      >
-    </v-card>
-  </div>
+        <v-card-title>{{ t(item.title) }}</v-card-title>
+        <v-card-text>{{ truncateText(t(item.description), 250) }}</v-card-text>
+        <NuxtLinkLocale
+          class="link transition rounded-lg pa-2"
+          :to="{
+            path: `/projects/${item.id}`,
+            query: {
+              title: item.title,
+              description: item.description,
+              link: item.link,
+              technologies: item.technologies,
+            },
+          }"
+        >
+          <v-icon size="small" icon="mdi-open-in-new" />
+        </NuxtLinkLocale>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 <script lang="ts" setup>
+const { t } = useI18n();
 const { data } = await useFetch("/api/projects");
+const truncateText = (text: string, length: number) => {
+  if (text.length <= length) {
+    return text;
+  }
+  return text.substring(0, length) + "...";
+};
 </script>
 <style scoped>
 .card {
   position: relative;
+  border: 3px solid #000;
+}
+
+.card:hover{
+  border-color: #469E49;
 }
 
 .link {
   position: absolute;
-  bottom: 2%;
+  bottom: 5%;
   right: 2%;
   text-decoration: none;
   border: 1px solid;
+  color: #469E49;
+  border: 1px solid #469E49;
+}
+.link:hover{
+  background-color: #469E49;
+  color: #fff;
 }
 </style>
