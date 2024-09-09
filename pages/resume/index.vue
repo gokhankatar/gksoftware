da030003ea282221a7cfc1a41de9ede8cd3efab9
Gökhan Katar <katar_gokhan@hotmail.com>
2024-09-09 18:24:21 +0300

    completed resume component

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -1,18 +1,17 @@
 <template>
   <hr class="my-10" />
-  <v-row class="mt-10 d-flex ga-2 justify-space-evenly align-center">
-    <img :src="profileImg" class="img d-none d-sm-flex" :width="300" cover />
+  <v-row class="mt-10 d-flex flex-wrap justify-space-evenly align-center ga-2">
+    <img :src="profileImg" class="img d-none d-sm-flex" width="250" cover />
     <img :src="profileImg" class="img d-flex d-sm-none" :width="200" cover />
-    <div
-      class="content d-flex flex-column align-center align-sm-start ga-1 ga-sm-3 mt-5 mt-sm-0"
-    >
+    <div class="content d-flex flex-column align-center align-sm-start ga-1 ga-sm-3 mt-5 mt-sm-0">
       <p class="name text-subtitle-1 text-md-h5 text-lg-h3">{{ data?.general.name }}</p>
-      <p class="title transition cursor-pointer text-h5 text-md-h4 text-lg-h1">
-        {{ data?.general.title }}
+      <p @mouseover="open" @mouseleave="close"
+        class="title-job transition cursor-pointer text-h5 text-md-h4 text-lg-h1">
+        <v-progress-circular v-if="isHover" color="primary" indeterminate></v-progress-circular>
+        {{ title }}
       </p>
       <p class="d-flex justify-center align-center ga-2">
-        <span class="name text-subtitle-1 text-md-h5 text-lg-h4"
-          >{{ data?.general.email }}
+        <span class="name text-subtitle-1 text-md-h5 text-lg-h4">{{ data?.general.email }}
         </span>
         <v-icon icon="mdi-email" size="large" />
       </p>
@@ -22,20 +21,17 @@
   <hr class="my-10" />
 
   <v-row class="my-5 d-flex flex-column ga-5">
-    <div class="title cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-school" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("education") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div
-      v-for="item of data?.education"
-      :key="item.degree"
-      class="d-flex justify-start align-center ga-2"
-    >
+    <div v-for="item of data?.education" :key="item.degree"
+      class="d-flex justify-center justify-sm-start align-center ga-2">
       <v-icon class="d-none d-sm-flex" icon="mdi-school" size="large" />
-      <p class="text-subtitle-1 text-md-h5">
+      <p class="text-subtitle-1 text-md-h5 text-center text-sm-start">
         {{
           t(item.institution) +
           " " +
@@ -50,120 +46,118 @@
   </v-row>
 
   <v-row class="mt-15 d-flex flex-column ga-5">
-    <div class="title cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-code-brackets" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("projects") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div
-      v-for="item of data?.projetcs"
-      :key="item.link"
-      class="d-flex justify-start align-center ga-2"
-    >
-      <v-icon icon="mdi-code-brackets" size="large" />
+    <div v-for="item of data?.projetcs" :key="item.link"
+      class="d-flex justify-center justify-sm-start align-center ga-2">
+      <v-icon class="d-none d-sm-flex" icon="mdi-code-brackets" size="large" />
       <a class="link text-subtitle-1 text-md-h5" :href="item.link" target="_blank">{{
         t(item.name)
-      }}</a>
+        }}</a>
     </div>
   </v-row>
 
   <v-row class="mt-15 d-flex flex-column ga-5">
-    <div class="title cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-certificate" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("certificates") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div
-      v-for="item of data?.certificates"
-      :key="item.name"
-      class="d-flex justify-start align-center ga-2"
-    >
-      <v-icon icon="mdi-certificate" size="large" />
-      <p class="text-subtitle-1 text-md-h5">{{ t(item.name) }}</p>
+    <div v-for="item of data?.certificates" :key="item.name"
+      class="d-flex justify-center justify-sm-start align-center ga-2">
+      <v-icon class="d-none d-sm-flex" icon="mdi-certificate" size="large" />
+      <p class="text-subtitle-1 text-md-h5 text-center text-sm-start">{{ t(item.name) }}</p>
     </div>
   </v-row>
 
   <v-row class="mt-15 d-flex flex-column ga-5">
-    <div class="title cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-network" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("experiences") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div
-      v-for="item of data?.experiences"
-      :key="item.experience"
-      class="d-flex justify-start align-center ga-2"
-    >
-      <v-icon icon="mdi-network" size="large" />
-      <p class="text-subtitle-1 text-md-h5">
+    <div v-for="item of data?.experiences" :key="item.experience"
+      class="d-flex justify-center justify-sm-start align-center ga-2">
+      <v-icon class="d-none d-sm-flex" icon="mdi-network" size="large" />
+      <p class="text-subtitle-1 text-md-h5 text-center text-sm-start">
         {{ t(item.experience) + " " + item.time }}
       </p>
     </div>
   </v-row>
 
   <v-row class="mt-15 d-flex flex-column ga-5">
-    <div class="title cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-language-javascript" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("skills") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div class="d-flex flex-wrap ga-2">
-      <v-chip
-        v-for="item of data?.skills"
-        :key="item"
-        class="skill transition cursor-pointer"
-        variant="outlined"
-        color="green-accent-3"
-        size="large"
-        :text="item"
-      />
+    <div class="d-flex flex-wrap justify-center justify-sm-start ga-2">
+      <v-chip v-for="item of data?.skills" :key="item" class="skill transition cursor-pointer" variant="outlined"
+        color="green-accent-3" size="large" :text="item" />
     </div>
   </v-row>
 
   <v-row class="mt-15 d-flex flex-column ga-5">
-    <div class="title-next cursor-pointer d-flex justify-start align-center ga-2 ga-md-5">
+    <div class="title-next cursor-pointer d-flex justify-center justify-sm-start align-center ga-2 ga-md-5">
       <v-icon icon="mdi-bullseye-arrow" size="x-large" />
       <p class="transition text-h5 text-md-h3">{{ t("what-next") }}</p>
     </div>
 
-    <hr class="line transition w-25" />
+    <hr class="line transition w-25 mx-auto mx-sm-0" />
 
-    <div class="d-flex flex-wrap ga-2">
-      <v-chip
-        v-for="item of data?.whatNext"
-        :key="item"
-        class="skill-next transition cursor-pointer"
-        variant="outlined"
-        color="light-blue"
-        size="large"
-        :text="item"
-      />
+    <div class="d-flex flex-wrap justify-center justify-sm-start ga-2">
+      <v-chip v-for="item of data?.whatNext" :key="item" class="skill-next transition cursor-pointer" variant="outlined"
+        color="light-blue" size="large" :text="item" />
     </div>
   </v-row>
 </template>
 
 <script lang="ts" setup>
 import profileImg from "~/assets/img/profile.jpg";
+import { ref } from "vue";
+
 const { t } = useI18n();
 const { data } = await useFetch("/api/resume");
+const title = ref("Front End Developer");
+const isHover = ref(false);
+
 useHead({
   title: `GK SOFTWARE | ${t("resume")}`,
 });
+
+const open = () => {
+  title.value = "Full Stack Developer";
+  isHover.value = true;
+};
+
+const close = () => {
+  title.value = "Front End Developer";
+  isHover.value = false;
+}
 </script>
+
 <style scoped>
 @import url("/assets/css/main.css");
 
 .img {
   border-radius: 50%;
 }
+
+.title-job:hover {
+  color: #2096F3;
+}
+
 .title:hover {
   color: #00c853;
 }
@@ -192,11 +186,13 @@ useHead({
 .link:hover::before {
   width: 100%;
 }
+
 .skill:hover {
   border-color: #00c853;
   background-color: #00c853;
   color: #fff !important;
 }
+
 .skill-next:hover {
   border-color: #03a9f4;
   background-color: #03a9f4;
--- server/api/resume.ts
+++ server/api/resume.ts
@@ -1,64 +1,72 @@
 export default eventHandler((event) => {
+  const cv = {
+    general: {
+      name: "Gokhan Katar",
+      title: "Front End Developer",
+      email: "katar_gokhan@hotmail.com",
+    },
+    education: [
+      {
+        institution: "insurance",
+        school: "baskent",
+        degree: "bachelor",
+        time: "2015-2020",
+      },
+      {
+        institution: "finance",
+        school: "atilim",
+        degree: "master",
+        time: "2022-2024",
+      },
+      {
+        institution: "computer-programming",
+        school: "anadolu",
+        degree: "bachelor",
+        time: "2024-",
+      },
+    ],
+    projetcs: [
+      { name: "academy", link: "https://esrefugurcelik.com/" },
+      { name: "get-country", link: "https://www.getcountry.net/" },
+      { name: "follow-yourself", link: "https://followyourself.net/" },
+      { name: "gksoftware", link: "https://gksoftware.net/" },
+    ],
+    certificates: [{ name: "vektorel" }],
+    experiences: [{ experience: "freelance", time: "2020-" }],
+    skills: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Typescript",
+      "VueJs",
+      "ViteJs",
+      "Vue-Router",
+      "Vuex",
+      "Pinia",
+      "NuxtJs",
+      "Firebase",
+      "Npm",
+      "Windows",
+      "MacOS",
+      "Git",
+      "Github",
+      "Vuetify",
+      "Gsap",
+      "i18n",
+      "Bootstrap",
+      "Webpack",
+      "Axios API",
+      "AJAX",
+      "Fetch API",
+      "Rest API",
+      "SwiperJs",
+      "Hosting",
+      "Domain",
+      "SaaS",
+      "PaaS",
+    ],
+    whatNext: ["NodeJs", "MySql", "Linux", "Swift"],
+  };
 
-    const cv = {
-        general: {
-            name: 'Gokhan Katar',
-            title: 'Front End Developer',
-            email: 'katar_gokhan@hotmail.com'
-        },
-        education: [
-            { institution: 'insurance', school: 'baskent', degree: 'bachelor', time: '2015-2020' },
-            { institution: 'finance', school: 'atilim', degree: 'master', time: '2022-2024' },
-            { institution: 'computer-programming', school: 'anadolu', degree: 'bachelor', time: '2024-' }
-        ],
-        projetcs: [
-            { name: 'academy', link: 'https://esrefugurcelik.com/' },
-            { name: 'get-country', link: 'https://www.getcountry.net/' },
-            { name: 'follow-yourself', link: 'https://followyourself.net/' },
-            { name: 'gksoftware', link: 'https://gksoftware.net/' },
-        ],
-        certificates: [
-            { name: 'vektorel' }
-        ],
-        experiences: [
-            { experience: 'freelance', time: '2020-' }
-        ],
-        skills: [
-            "HTML",
-            "CSS",
-            "Javascript",
-            "Typescript",
-            "VueJs",
-            "NuxtJs",
-            "Firebase",
-            "Npm",
-            "Windows",
-            "MacOS",
-            "Git",
-            "Github",
-            "Vuetify",
-            "Gsap",
-            "i18n",
-            "Bootstrap",
-            "ViteJs",
-            "Webpack",
-            "Axios API",
-            "AJAX",
-            "Fetch API",
-            "Rest API",
-            "SwiperJs",
-            "Hosting",
-            "Domain",
-            "SaaS",
-            "PaaS"
-        ],
-        whatNext: [
-            "NodeJs",
-            "MySql",
-            "Linux",
-            "Swift"
-        ]
-    }
-
-    return cv;
-})
+  return cv;
+});
